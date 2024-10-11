fa384a5654d9213c263db03ce3d2f843c4b03bab
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2024-10-11 16:44:33 -0700

    Completed Step 1: 🪨

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Boulder CLicker";
+const gameName = "Boulder Pusher";
 document.title = gameName;
+
+const button = document.createElement("button");
+button.innerHTML = "🪨";
+app.append(button);
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
