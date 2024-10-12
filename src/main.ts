fc5d5a80348453e1f4e05f6f80f05df1f5e39103
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2024-10-11 18:04:47 -0700

    Completed Step 2

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,29 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+let counter: number = 0;
+
 const gameName = "Boulder Pusher";
 document.title = gameName;
+
+
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
 
 const button = document.createElement("button");
 button.innerHTML = "🪨";
 app.append(button);
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
+button.addEventListener("click", pushBoulder);
+
+const div = document.createElement("dive");
+div.innerHTML = counter.toString();
+app.append(div);
+
+function pushBoulder(){
+    counter++;
+    div.innerHTML = counter.toString();
+}
