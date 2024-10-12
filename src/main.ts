c6927fb45656f148168e117c53a11c8291995e00
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2024-10-11 19:41:34 -0700

    Completed Step 4

--- src/main.ts
+++ src/main.ts
@@ -7,28 +7,35 @@ let counter: number = 0;
 const gameName = "Boulder Pusher";
 document.title = gameName;
 
-
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
-
 
 const button = document.createElement("button");
 button.innerHTML = "🪨";
 app.append(button);
 
-button.addEventListener("click", pushBoulder);
+button.addEventListener("click", function() {pushBoulder(1)});
 
 const div = document.createElement("dive");
 div.innerHTML = counter.toString();
 app.append(div);
 
-const intervalID = setInterval(pushBoulder, 1000);
-console.log(intervalID);
 
+let  zero: number = performance.now();
+let timeElapsed: number;
 
+function incrementBoulder(){
+    timeElapsed = performance.now() - zero;
+    zero = performance.now();
+    pushBoulder(timeElapsed/1000);
+    requestAnimationFrame(incrementBoulder);
+}
 
-function pushBoulder(){
-    counter++; 
-    div.innerHTML = counter.toString();
+requestAnimationFrame(incrementBoulder);
+
+function pushBoulder(increment: number) {
+  counter += increment;
+  
+  div.innerHTML = Math.trunc(counter).toString();
 }
