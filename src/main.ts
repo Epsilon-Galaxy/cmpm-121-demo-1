b7f6fef5166ace7ef933c9462c2ae9392e7f6112
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2024-10-16 01:09:43 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+//Number of meters boulder is pushed
 let counter: number = 0;
+
 
 const gameName = "Boulder Pusher";
 document.title = gameName;
@@ -11,10 +13,20 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+//Button to click to push boulder
 const button = document.createElement("button");
 button.innerHTML = "🪨";
 app.append(button);
 
+const upgrade_1 = document.createElement("button");
+upgrade_1.innerHTML = "Buy a Sisyphus";
+app.append(upgrade_1);
+
+upgrade_1.addEventListener("click", function() {buyUpgrade(1)})
+upgrade_1.disabled = true;
+
+
+//On button click perform push Boulder
 button.addEventListener("click", function() {pushBoulder(1)});
 
 const div = document.createElement("dive");
@@ -25,10 +37,19 @@ app.append(div);
 let  zero: number = performance.now();
 let timeElapsed: number;
 
+//Growth rate of boulder pushes
+let growthRate: number = 0;
+
+//increments boulder by growth rate
 function incrementBoulder(){
     timeElapsed = performance.now() - zero;
     zero = performance.now();
-    pushBoulder(timeElapsed/1000);
+    pushBoulder(growthRate * (timeElapsed/1000));
+
+    if(counter >= 10){
+      upgrade_1.disabled = false;
+    }
+
     requestAnimationFrame(incrementBoulder);
 }
 
@@ -38,4 +59,16 @@ function pushBoulder(increment: number) {
   counter += increment;
   
   div.innerHTML = Math.trunc(counter).toString();
+}
+
+
+function buyUpgrade(upgrade: number){
+  switch(upgrade){
+    case(1):
+      counter -= 10;
+      if(counter < 10){
+        upgrade_1.disabled = true;
+      }
+      growthRate += 1;
+  }
 }
