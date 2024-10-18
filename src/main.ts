19b2a0166d5f6e9ca9effe82eca7755e3fbb043c
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2024-10-17 23:19:01 -0700

    formatting changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 //Number of meters boulder is pushed
 let counter: number = 0;
 
-
 const gameName = "Boulder Pusher";
 document.title = gameName;
 
@@ -22,51 +21,52 @@ const upgrade_1 = document.createElement("button");
 upgrade_1.innerHTML = "Buy a Sisyphus";
 app.append(upgrade_1);
 
-upgrade_1.addEventListener("click", function() {buyUpgrade(1)})
+upgrade_1.addEventListener("click", function () {
+  buyUpgrade(1);
+});
 upgrade_1.disabled = true;
 
-
 //On button click perform push Boulder
-button.addEventListener("click", function() {pushBoulder(1)});
+button.addEventListener("click", function () {
+  pushBoulder(1);
+});
 
 const div = document.createElement("dive");
 div.innerHTML = counter.toString();
 app.append(div);
 
-
-let  zero: number = performance.now();
+let zero: number = performance.now();
 let timeElapsed: number;
 
 //Growth rate of boulder pushes
 let growthRate: number = 0;
 
 //increments boulder by growth rate
-function incrementBoulder(){
-    timeElapsed = performance.now() - zero;
-    zero = performance.now();
-    pushBoulder(growthRate * (timeElapsed/1000));
+function incrementBoulder() {
+  timeElapsed = performance.now() - zero;
+  zero = performance.now();
+  pushBoulder(growthRate * (timeElapsed / 1000));
 
-    if(counter >= 10){
-      upgrade_1.disabled = false;
-    }
+  if (counter >= 10) {
+    upgrade_1.disabled = false;
+  }
 
-    requestAnimationFrame(incrementBoulder);
+  requestAnimationFrame(incrementBoulder);
 }
 
 requestAnimationFrame(incrementBoulder);
 
 function pushBoulder(increment: number) {
   counter += increment;
-  
+
   div.innerHTML = Math.trunc(counter).toString();
 }
 
-
-function buyUpgrade(upgrade: number){
-  switch(upgrade){
-    case(1):
+function buyUpgrade(upgrade: number) {
+  switch (upgrade) {
+    case 1:
       counter -= 10;
-      if(counter < 10){
+      if (counter < 10) {
         upgrade_1.disabled = true;
       }
       growthRate += 1;
